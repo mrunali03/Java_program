d4f0563b725a0d9888007ea54e59eddb0f46ab2e
mrunali <64697169+mrunali03@users.noreply.github.com>
2020-07-14 17:41:56 +0530

    add switch case statment
    add switch case statment

---
+++ usecase4.java
@@ -0,0 +1,34 @@
+public class usecase4 {
+
+     public static void main(String[] args) {
+	 System.out.println("Welcome to employee wage computation Problem..");
+
+	 public static final int is_fullTime=1;
+         public static final int is_PartTime=2;
+         public static final int ratePerHrs=20;
+
+        public static void main(String[] args)
+        {
+                //variables
+                int empHrs=0;
+                int empWage=0;
+                //computation
+                double randomCheck=Math.floor(Math.random()*10)%3;
+
+                switch ((int)randomCheck)
+                {
+                case is_fullTime:
+                                empHrs=8;
+                                break;
+                case is_PartTime:
+                                empHrs=4;
+                                break;
+                default:
+                        empHrs=0;
+                        break;
+                }
+                empWage=ratePerHrs*empHrs;
+                System.out.println("Employee Wage : "+empWage);
+
+	}
+}
